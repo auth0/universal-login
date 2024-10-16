a87725ef7aebc1dc20b9d92db96547423e1f6663
Nandan Bhat <nandan.bhat@okta.com>
2024-10-16 21:30:16 +0530

    Fixing typedoc script command

--- typedoc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  out: "./docs/",
-  excludePrivate: false,
-  excludeExternals: false,
-  hideGenerator: true,
-  readme: "./README.md",
-  entryPoints: ["packages/ul-javascript/src"],
-  tsconfig: "tsconfig.typedoc.json",
-};
