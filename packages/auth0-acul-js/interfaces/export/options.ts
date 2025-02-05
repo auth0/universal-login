f196a8d44d627ae2cc9ba098272b42ab94fac1a3
Gyanesh Gouraw <gyanesh.gouraw@okta.com>
2025-02-05 16:16:43 +0530

    Fixed lint related errors

--- packages/auth0-acul-js/interfaces/export/options.ts
+++ packages/auth0-acul-js/interfaces/export/options.ts
@@ -11,7 +11,7 @@ export type { SignupOptions, SocialSignupOptions } from '../screens/signup-id';
 export type { SignupPasswordOptions } from '../screens/signup-password';
 export type { CustomOptions } from '../common';
 export type { LoginOptions as LoginPayloadOptions, SocialLoginOptions as SocialLoginPayloadOptions } from '../screens/login';
-export type { SignupOptions as SignupPayloadOptions, SocialSignupOptions as SocialSignupPayloadOptions  } from '../screens/signup';
+export type { SignupOptions as SignupPayloadOptions, SocialSignupOptions as SocialSignupPayloadOptions } from '../screens/signup';
 export type { ResetPasswordEmailOptions } from '../screens/reset-password-email';
 export type { ResetPasswordRequestOptions } from '../screens/reset-password-request';
 export type { ResetPasswordOptions } from '../screens/reset-password';
