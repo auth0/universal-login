cb966537e7c72a8672c45a84f36370001b4f2051
Nandan Bhat <nandan.bhat@okta.com>
2025-03-20 23:18:09 +0530

    Adding ACUL support for organization-picker screen

--- packages/auth0-acul-js/interfaces/export/screen-properties.ts
+++ packages/auth0-acul-js/interfaces/export/screen-properties.ts
@@ -39,3 +39,4 @@ export type { MfaOtpEnrollmentQrMembers } from '../screens/mfa-otp-enrollment-qr
 export type { MfaOtpEnrollmentCodeMembers } from '../screens/mfa-otp-enrollment-code';
 export type { ResetPasswordMfaOtpChallengeMembers } from '../screens/reset-password-mfa-otp-challenge';
 export type { OrganizationSelectionMembers } from '../screens/organization-selection';
+export type { OrganizationPickerMembers } from '../screens/organization-picker';
