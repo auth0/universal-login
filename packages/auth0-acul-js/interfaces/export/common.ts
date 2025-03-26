b30d71a5e388944e4522f13a1a55df11be0d1125
Subhankar Maiti <35273200+subhankarmaiti@users.noreply.github.com>
2025-03-26 17:25:34 +0530

    Export `ShortEntity` type from user model for docs (#61)
    * Export `ShortEntity` type from user model for docs
    
    * Export `ShortEntity` type from user model in common interface

--- packages/auth0-acul-js/interfaces/export/common.ts
+++ packages/auth0-acul-js/interfaces/export/common.ts
@@ -2,3 +2,4 @@ export type { CaptchaContext, PhonePrefix } from '../models/screen';
 export type { Connection, EnterpriseConnection, PasswordPolicy, UsernamePolicy, Error, PasswordComplexityRule } from '../models/transaction';
 export type { BrandingSettings, BrandingThemes } from '../models/branding';
 export type { CustomOptions } from '../common/index';
+export type { ShortEntity } from '../models/user';
