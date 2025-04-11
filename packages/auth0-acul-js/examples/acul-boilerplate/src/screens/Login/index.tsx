cbce927234f587f4eadefce53f0830103593e845
Amber Kamboj <amber.kamboj77@gmail.com>
2025-04-11 18:30:30 +0530

    fix: update password placeholder text to include 'test-acul'

--- packages/auth0-acul-js/examples/acul-boilerplate/src/screens/Login/index.tsx
+++ packages/auth0-acul-js/examples/acul-boilerplate/src/screens/Login/index.tsx
@@ -65,7 +65,7 @@ const LoginScreen: React.FC = () => {
       texts.usernameOrEmailPlaceholder ||
       texts.phoneOrUsernameOrEmailPlaceholder ||
       "Username or Email",
-    passwordPlaceholder: texts.passwordPlaceholder.concat("test") || "Password",
+    passwordPlaceholder: texts.passwordPlaceholder.concat("test-acul") || "Password",
     captchaPlaceholder:
       texts.captchaCodePlaceholder || "Enter the code shown above",
     resetPasswordLink,
