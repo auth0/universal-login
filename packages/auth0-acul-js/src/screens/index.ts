b3955567c25c7a7cf2ae3d006093046b3543ea12
Subhankar Maiti <subhankar.maiti@okta.com>
2025-05-13 00:44:00 +0530

    feat: add email verification result and login email verification screens

--- packages/auth0-acul-js/src/screens/index.ts
+++ packages/auth0-acul-js/src/screens/index.ts
@@ -60,3 +60,5 @@ export { default as MfaRecoveryCodeChallengeNewCode } from './mfa-recovery-code-
 export { default as Logout } from './logout';
 export { default as LogoutAborted } from './logout-aborted';
 export { default as LogoutComplete } from './logout-complete';
+export { default as EmailVerificationResult } from './email-verification-result';
+export { default as LoginEmailVerification } from './login-email-verification';
