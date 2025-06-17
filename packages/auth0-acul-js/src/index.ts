b965c4503477126f65388fac6e03b40f5fe96568
Amit Singh <amitk.singh@okta.com>
2025-06-17 09:41:33 +0530

    feat: move getError function to utils and update exports

--- packages/auth0-acul-js/src/index.ts
+++ packages/auth0-acul-js/src/index.ts
@@ -1,23 +1,12 @@
 import { BaseContext } from './models/base-context';
-
-import type { Error } from '../interfaces/models/transaction';
-
 export * from './screens';
 export * from '../interfaces/export/base-properties';
 export * from '../interfaces/export/screen-properties';
 export * from '../interfaces/export/options';
 export * from '../interfaces/export/extended-types';
 export * from '../interfaces/export/common';
+export * from './utils/get-error';
 
 export function getCurrentScreen(): string | null {
   return new BaseContext().getContext('screen')?.name ?? null;
-}
-
-export function getError(): Error[] {
-  try {
-    const context = new BaseContext();
-    return context.transaction?.errors ?? [];
-  } catch {
-    return [];
-  }
 }
