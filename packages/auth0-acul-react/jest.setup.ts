d0d13f4d054ecb769de02a003a170875f76b0407
Amit Singh <amitk.singh@okta.com>
2025-09-24 21:26:05 +0530

    fix: enforce SDK name and version in Jest setup

--- packages/auth0-acul-react/jest.setup.ts
+++ packages/auth0-acul-react/jest.setup.ts
@@ -6,9 +6,9 @@ declare global {
   var __SDK_VERSION__: string;
 }
 
-// Set SDK name and version
-globalThis.__SDK_NAME__ = global.__SDK_NAME__ || '@auth0/auth0-acul-react';
-globalThis.__SDK_VERSION__ = global.__SDK_VERSION__ || '0.1.0';
+// Set SDK name and version - Force React SDK identity
+globalThis.__SDK_NAME__ = '@auth0/auth0-acul-react';
+globalThis.__SDK_VERSION__ = '0.1.0';
 
 // Cleanup after each test to prevent memory leaks and state pollution
 // This is a React testing best practice
