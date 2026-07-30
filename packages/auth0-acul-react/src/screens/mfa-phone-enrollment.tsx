8d1dab10c84617c7d88f86f9b5cd837bbad259cd
Amit Singh <amitk.singh@okta.com>
2026-07-30 11:13:49 +0530

    refactor: limit useCountryCodes to login, login-id, signup, signup-id

--- packages/auth0-acul-react/src/screens/mfa-phone-enrollment.tsx
+++ packages/auth0-acul-react/src/screens/mfa-phone-enrollment.tsx
@@ -28,7 +28,6 @@ export const {
   usePrompt,
   useScreen,
   useTransaction,
-  useCountryCodes,
   useUntrustedData,
 } = factory;
 
--- packages/auth0-acul-react/src/screens/mfa-sms-enrollment.tsx
+++ packages/auth0-acul-react/src/screens/mfa-sms-enrollment.tsx
@@ -27,7 +27,6 @@ export const {
   usePrompt,
   useScreen,
   useTransaction,
-  useCountryCodes,
   useUntrustedData,
 } = factory;
 
